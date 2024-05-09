886a6148d45f0d57212b2a8766adfa7edd752faf
tifufu <dematasjoshua@hotmail.com>
2024-05-09 18:36:42 +0200

    write http status 200 and close connection

--- app/server.go
+++ app/server.go
@@ -15,9 +15,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = listener.Accept()
+	connection, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	resp := []byte(`HTTP/1.1 200 OK\r\n\r\n`)
+	_, err = connection.Write(resp)
+	if err != nil {
+		fmt.Println("Failed to write to connection")
+		os.Exit(1)
+	}
+	err = connection.Close()
+	if err != nil {
+		fmt.Println("Failed to close connection")
 		os.Exit(1)
 	}
 }
