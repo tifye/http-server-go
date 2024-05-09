499b84f0f79b1f15722eb31d050418c6558a815b
tifufu <dematasjoshua@hotmail.com>
2024-05-10 01:30:28 +0200

    Loop in goroutine picking up connections and sending through channel

--- app/server.go
+++ app/server.go
@@ -2,14 +2,72 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 )
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
+	router := setupRouter()
+
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	connChan := make(chan net.Conn)
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				cancel()
+				return
+			}
+			connChan <- conn
+		}
+	}()
+
+	for {
+		select {
+		case conn := <-connChan:
+			go serve(ctx, router, conn)
+		case <-ctx.Done():
+			fmt.Println("Exiting listener goroutine")
+			return
+		}
+	}
+}
+
+func serve(ctx context.Context, router *Router, conn net.Conn) {
+	defer conn.Close()
+
+	request, err := parseRequest(conn)
+	if err != nil {
+		fmt.Printf("Failed to parse request got %s\n", err)
+		os.Exit(1)
+	}
+
+	handler := router.findHandler(&request)
+	if handler == nil {
+		fmt.Printf("No handler for %s %s", request.method, request.path)
+		return
+	}
+	for key, value := range request.params {
+		fmt.Printf("%s: %s\n", key, value)
+	}
+
+	handler(&request, conn)
+}
+
+func setupRouter() *Router {
 	router := newRouter()
 	router.GET("/", func(req *Request, res ResponseWriter) {
 		res.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
@@ -51,40 +109,5 @@ func main() {
 		buf.WriteString(userAgent)
 		buf.WriteTo(res)
 	})
-
-	listener, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	connection, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer func() {
-		err = connection.Close()
-		if err != nil {
-			fmt.Println("Failed to close connection")
-			os.Exit(1)
-		}
-	}()
-
-	request, err := parseRequest(connection)
-	if err != nil {
-		fmt.Printf("Failed to parse request got %s\n", err)
-		os.Exit(1)
-	}
-
-	handler := router.findHandler(&request)
-	if handler == nil {
-		fmt.Printf("No handler for %s %s", request.method, request.path)
-		return
-	}
-	for key, value := range request.params {
-		fmt.Printf("%s: %s\n", key, value)
-	}
-
-	handler(&request, connection)
+	return &router
 }
